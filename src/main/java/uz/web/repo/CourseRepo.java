a20d3566e475d2a89a221edb018f27c7ad714be1
Jamshid <126463858+ghost-device@users.noreply.github.com>
2024-07-24 12:03:23 +0500

    Update CourseRepo.java

--- src/main/java/uz/web/repo/CourseRepo.java
+++ src/main/java/uz/web/repo/CourseRepo.java
@@ -7,12 +7,12 @@ import java.util.List;
 import java.util.UUID;
 
 public class CourseRepo extends BaseRepo<CourseEntity> {
-
     public List<CourseEntity> findAllCourse() {
         String findAll = "select c from CourseEntity c";
         TypedQuery<CourseEntity> query = entityManager.createQuery(findAll, CourseEntity.class);
         return query.getResultList();
     }
+    
     public List<CourseEntity> findCoursesByUser(UUID id) {
         return entityManager.createQuery("select u.course from CoursesOfUsersEntity u where u.user.id = :id", CourseEntity.class)
                 .setParameter("id", id)
